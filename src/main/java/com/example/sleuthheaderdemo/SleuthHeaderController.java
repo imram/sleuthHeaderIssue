753b7a57ca41ffb81df887d010a451baece5439a
ramgupta <ram.gupta@aa.com>
2018-01-11 10:13:55 -0600

    Added another downline call to Bluemix Same Service Different EP

--- src/main/java/com/example/sleuthheaderdemo/SleuthHeaderController.java
+++ src/main/java/com/example/sleuthheaderdemo/SleuthHeaderController.java
@@ -2,20 +2,58 @@ package com.example.sleuthheaderdemo;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.sleuth.annotation.NewSpan;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class SleuthHeaderController {
     public static final Logger LOGGER = LoggerFactory.getLogger(SleuthHeaderController.class);
+    public static final String HTTP_LOCALHOST_URL = "http://localhost:9090/";
+    public static final String HTTP_BLUEMIX_URL = "https://sleuth-header-demo.mybluemix.net/";
 
+    @Value("${home.url}")
+    String url;
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+
+
+    @NewSpan
     @GetMapping(value = "/hello")
     public String hello(@RequestParam(name = "name") String name,
             @RequestHeader(name = "X-B3-TraceId", required = false) String traceId) {
 
-        LOGGER.info("Call Received");
-        return "HELLO" + name;
+        LOGGER.info(String.format("%s Forwarding to Home", name));
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        //headers.set("X-B3-TraceId", traceId);
+
+        HttpEntity<String> entity = new HttpEntity<String>(headers);
+
+        ResponseEntity<String> response =  restTemplate.exchange(url, HttpMethod.GET, entity, String.class);
+        return response.getBody();
     }
+
+    @NewSpan
+    @GetMapping(value = "/")
+    public String home() {
+
+        LOGGER.info("Call is at Home");
+        return "Welcome There!!";
+    }
+
+
 }
--- src/main/java/com/example/sleuthheaderdemo/SleuthHeaderDemoApplication.java
+++ src/main/java/com/example/sleuthheaderdemo/SleuthHeaderDemoApplication.java
@@ -2,6 +2,8 @@ package com.example.sleuthheaderdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class SleuthHeaderDemoApplication {
@@ -9,4 +11,9 @@ public class SleuthHeaderDemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SleuthHeaderDemoApplication.class, args);
 	}
+
+    @Bean
+    public RestTemplate aaRestTemplate() {
+        return new RestTemplate();
+    }
 }
